54ca6ccc3182e98c004535cc935a6c7231de5dd3
MTaimoor <mtaimoor@cloudchef.tech>
2023-01-21 15:31:31 +0200

    Configs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { AppInjector } from './app/core/services/app-injector.service';
 import { environment } from './environments/environment';
+import './app/shared/utils/date.utils';
+import './app/shared/utils/array.utils';
+import './app/shared/utils/html.utils';
 
 if (environment.production) {
   enableProdMode();
